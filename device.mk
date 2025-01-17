b818ca0bbb79551247329b9aa64c6c1b61233d41
LuK1337 <priv.luk@gmail.com>
2025-01-17 17:06:51 +0100

    salaa: Switch to common lights HAL
    Change-Id: If515e7c3f6e5b6895076f1d8381fb2c22bd1b3de

--- device.mk
+++ device.mk
@@ -140,7 +140,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.salaa
+    android.hardware.light-service.lineage
 
 # Lineage Health
 PRODUCT_PACKAGES += \
