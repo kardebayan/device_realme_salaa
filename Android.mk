8f2fb8a70e2cc12c4a8fa82c15d9037bd6befa51
Debayan Kar <kardebayan3@gmail.com>
2026-04-22 17:30:18 +0000

    Revert "salaa: Remove oppo logical partitions when necessary"
    This reverts commit b855bbad3f49d2b39028e115a367387abd671e0c.
    
    Change-Id: I74e1d2415112cb792163f0545a836f75d5cd37ef

--- Android.mk
+++ Android.mk
@@ -7,9 +7,6 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter salaa, $(TARGET_DEVICE)),)
-
-$(call add-radio-file,dynamic-remove-oppo)
-
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 endif
